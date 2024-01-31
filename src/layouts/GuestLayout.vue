063eabbf02f1ff39c15aa7aa6188460e8797c21d
Nguyen Huu Huan <huan.nguyen2@kiaisoft.com.vn>
2024-01-31 17:47:59 +0700

    feature: sign in ui

---
+++ src/layouts/GuestLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <router-view />
+    </div>
+</template>
+<script>
+// import AppHeader from '../components/AppHeader.vue'
+// import AppFooter from '../components/AppFooter.vue'
+export default {
+}
+
+</script>
