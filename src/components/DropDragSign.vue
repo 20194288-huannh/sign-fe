8e77edf39fa42a6687220974bfac6149a6184a52
Quang Anh <anh.tran@kiaisoft.com>
2024-03-11 17:08:23 +0700

    add drag and drop

--- src/components/DropDragSign.vue
+++ src/components/DropDragSign.vue
@@ -1,27 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue';
+import { ref } from 'vue'
+import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue'
 
 const props = defineProps({
-    width: Number,
-    height: Number,
-    top: Number,
-    left: Number
+  width: Number,
+  height: Number,
+  top: Number,
+  left: Number,
+  text: String
 })
 
-const emit = defineEmits(['resize'])
-const resize = (newRect: { width: number; height: number; top: number; left: number; }) => {
-    emit('resize', newRect)
+const emit = defineEmits(['resize', 'dragStop'])
+const resize = (newRect: { width: number; height: number; top: number; left: number }) => {
+  emit('resize', newRect)
 }
-</script >
+
+const onDragstop = () => {
+  emit('dragStop', false)
+}
+</script>
 
 <template>
-    <div>
-        <VueDragResize :w="100" :h="40" :z="100" :isResizable="false" v-on:resizing="resize" v-on:dragging="resize">
-            <!-- <h3>Hello World!</h3>
-            <p>{{ props.top }} х {{ props.left }} </p>
-            <p>{{ props.width }} х {{ props.height }}</p> -->
-            <div class="h-[40px] rounded-sm border-neutral-400 bg-white shadow-inner ring-1">Hello</div>
-        </VueDragResize>
-    </div>
+  <div>
+    <VueDragResize
+      :w="100"
+      :h="40"
+      :z="100"
+      :isResizable="false"
+      v-on:resizing="resize"
+      v-on:dragging="resize"
+      @dragstop="onDragstop"
+    >
+      <div
+        class="h-[40px] rounded-sm border-neutral-400 bg-white shadow-inner ring-1 flex items-center justify-center"
+      >
+        {{ text }}
+      </div>
+    </VueDragResize>
+  </div>
 </template>
--- src/components/MainStep/PrepareDocument/index.vue
+++ src/components/MainStep/PrepareDocument/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="relative">
-    <div class="flex items-center justify-center bg-[#f4f4f4] h-[55px]">
-
-      <DropDragSign :width="position1.width" :height="position1.height" :top="position1.top" :left="position1.left" @resize="resize"/>
-      <DropDragSign :width="position2.width" :height="position2.height" :top="position2.top" :left="position2.left" @resize="resize"/>
+    <div class="flex items-center bg-[#f4f4f4] h-[55px]">
+      <div class="relative h-[55px] min-w-28 mt-4 ml-2" v-for="it in arrType" :key="it.type">
+        <DropDragSign
+          v-for="(item, idx) in arrResize"
+          :key="item.id"
+          :width="item.width"
+          :height="item.height"
+          :top="item.top"
+          :left="item.left"
+          @resize="(newRect) => resize(newRect, idx, it.type)"
+          :text="it.text"
+          @drag-stop="dragStop(it.type)"
+        />
+      </div>
+      <!-- <DropDragSign :width="position2.width" :height="position2.height" :top="position2.top" :left="position2.left" @resize="resize"/> -->
     </div>
     <div id="pageContainer">
-      <div id="viewer" class="pdfViewer">
-        
-      </div>
+      <div id="viewer" class="pdfViewer"></div>
     </div>
   </div>
 </template>
@@ -27,12 +36,111 @@ onMounted(() => {
   getPdf()
 })
 
-const resize = (newRect: { width: number; height: number; top: number; left: number; }) => {
-  console.log(newRect)
-  position.value.width = newRect.width
-  position.value.height = newRect.height
-  position.value.top = newRect.top
-  position.value.left = newRect.left
+const dragger = ref(false)
+
+const arrResize = ref<any>([
+  {
+    width: 100,
+    height: 40,
+    top: 0,
+    left: 0
+  }
+])
+
+const arrType = ref<any>([
+  {
+    type: 'input',
+    text: 'hello',
+    arrSize: [
+      {
+        width: 100,
+        height: 40,
+        top: 0,
+        left: 0
+      }
+    ]
+  },
+  {
+    type: 'radio',
+    text: 'signature',
+    arrSize: [
+      {
+        width: 100,
+        height: 40,
+        top: 0,
+        left: 0
+      }
+    ]
+  },
+  {
+    type: 'date',
+    text: 'date',
+    arrSize: [
+      {
+        width: 100,
+        height: 40,
+        top: 0,
+        left: 0
+      }
+    ]
+  }
+])
+
+const dragStop = (type: string) => {
+  console.log(type)
+  dragger.value = false
+}
+
+const resize = (
+  newRect: { width: number; height: number; top: number; left: number },
+  idx: number,
+  item: string
+) => {
+  dragger.value = true
+  const _arrResize = [...arrResize.value]
+  _arrResize[idx] = {
+    width: newRect.width,
+    height: newRect.height,
+    top: newRect.top,
+    left: newRect.left
+  }
+  arrResize.value[idx] = _arrResize[idx]
+  if (
+    (newRect.top !== 0 || newRect.left !== 0) &&
+    !arrResize.value.find((e) => e.top == 0 && e.left == 0)
+  ) {
+    arrResize.value.push({
+      width: 100,
+      height: 40,
+      top: 0,
+      left: 0
+    })
+  }
+  // if (arrResize.value.filter((e) => e.top == 0 && e.left == 0).length > 1) {
+  //   arrResize.value = arrResize.value.reduce((pre, cur) => {
+  //     console.log(12332131232)
+  //     if (!pre) {
+  //       return [...(pre || []), cur]
+  //     }
+
+  //     if (pre.find((e) => e.top == cur.top || e.left == cur.left)) {
+  //       console.log(123321)
+  //       return [
+  //         ...pre,
+  //         {
+  //           width: 100,
+  //           height: 40,
+  //           top: 0,
+  //           left: 0
+  //         }
+  //       ]
+  //     }
+
+  //     if (cur.top !== 0 || cur.left !== 0) {
+  //       return [...pre, cur]
+  //     }
+  //   }, [])
+  // }
 }
 
 const position1 = ref({
